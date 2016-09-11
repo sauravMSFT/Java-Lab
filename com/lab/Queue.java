91ce6dc29f9c5af6b08a5ecfb58781b1d87a8fd6
sauravMSFT <sauravsahu.12@live.com>
2016-09-11 19:38:06 +0530

    Added Queue.java

---
+++ com/lab/Queue.java
@@ -0,0 +1,109 @@
+package com.lab;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by saurav_msft on 22/8/16.
+ */
+public class Queue<T>
+{
+    private ArrayList<T> queue;
+    private int count, maxSize;
+
+
+    public Queue(int size)
+    {
+        queue = new ArrayList<>();
+        count = 0;
+        maxSize = size;
+    }
+
+    public void Enqueue(T item) throws Exception
+    {
+        if (count == maxSize)
+            throw new Exception("Queue Overflow... Can't enqueue anymore items.");
+        queue.add(item);
+        count++;
+    }
+
+    public T Dequeue() throws Exception
+    {
+        if (queue.isEmpty())
+            throw new Exception("Queue Underflow... Can't dequeue item.");
+
+        count--;
+        return queue.remove(0);
+    }
+
+    public void Display()
+    {
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuffer rep = new StringBuffer("[ ");
+        for (int i = 0; i < count; ++i) rep.append(String.format("%1$s  ", queue.get(i)));
+        rep.append("] <--");
+        return rep.toString();
+    }
+
+    private static void demo() throws Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter the size of the queue: ");
+        int size = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new Queue<>(size);
+
+        while (true)
+        {
+            System.out.println("Queue Operations:\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit");
+            System.out.print("Enter your choice: ");
+
+
+            int choice = Integer.parseInt(br.readLine());
+            int item;
+            switch (choice)
+            {
+                case 1:
+                    System.out.println("Enter the number to Enqueue: ");
+                    item = Integer.parseInt(br.readLine());
+
+                    try
+                    {
+                        queue.Enqueue(item);
+                        System.out.println(item + " was enqueued.");
+                    }
+                    catch (Exception e) { System.out.println(e.getMessage()); }
+
+                    break;
+
+                case 2:
+                    try
+                    {
+                        item = queue.Dequeue();
+                        System.out.println(item + " was dequeued.");
+                    }
+                    catch (Exception e) { System.out.println(e.getMessage()); }
+                    break;
+                case 3:
+                    System.out.println("The queue contains: ");
+                    queue.Display();
+                    break;
+                case 4:
+                    return;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception
+    {
+        demo();
+    }
+}
---
+++ com/lab/quadratic/Equation.java
@@ -0,0 +1,114 @@
+package com.lab.quadratic;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+/**
+ * Created by saurav_msft on 7/27/2016.
+ */
+public class Equation
+{
+    private double a, b, c;
+    private double det;
+
+    public Equation(double a, double b, double c) throws Exception
+    {
+        //ALT CODE for 2 SUP is 253
+        if (a == 0.0) throw new Exception("coefficient of x² cannot be zero...");
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        det = b * b - 4 * a * c;
+    }
+
+    public double Discriminant()
+    {
+        return det;
+    }
+
+    public Complex[] Solution()
+    {
+        double s1, s2, det1= Math.sqrt(this.det);
+
+        if (det >= 0)
+        {
+            s1 = (-b + det1) / (2 * a);
+            s2 = (-b - det1) / (2 * a);
+
+            return new Complex[]{new Complex(s1, 0), new Complex(s2, 0)};
+        } else
+        {
+            det1 = Math.sqrt(-det);
+            s1 = -b / (2 * a);
+            s2 = Math.sqrt(-det) / (2 * a);
+
+            return new Complex[]{new Complex(s1, s2), new Complex(s1, -s2)};
+        }
+    }
+
+    private String Value(double val)
+    {
+        return (val >= 0) ? ("+ " + val) : ("- " + Double.toString(Math.abs(val)));
+    }
+
+    public String toString()
+    {
+        return Value(a) + "x² " + Value(b) + "x " + Value(c) + " = 0";
+    }
+
+    private static void demo()
+    {
+        PrintWriter p = new PrintWriter(System.out, true);
+        BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+        Equation eq = null;
+        do
+        {
+            p.println("Enter the coefficients (a, b, c) in ax² + bx + c = 0");
+            try
+            {
+                double a, b, c;
+                p.print("a = ");
+                p.flush();
+                a = Double.parseDouble(s.readLine());
+
+                p.print("b = ");
+                p.flush();
+                b = Double.parseDouble(s.readLine());
+
+                p.print("c = ");
+                p.flush();
+                c = Double.parseDouble(s.readLine());
+
+                eq = new Equation(a, b, c);
+            } catch (Exception e)
+            {
+                p.println(e.getMessage());
+                p.println("Try again.");
+            }
+        } while (eq == null);
+
+        p.println("The given equation: " + eq);
+        String str;
+        Complex[] solutions = eq.Solution();
+
+        if (eq.Discriminant() > 0)
+            str = "two real and distinct roots.";
+        else if (eq.Discriminant() == 0)
+            str = "real but equal roots";
+        else
+            str = "complex roots";
+
+        p.println("The above equation has " + str);
+
+        p.println("The roots are");
+        p.println("x = " + solutions[0]);
+        p.println("x = " + solutions[1]);
+    }
+
+    public static void main(String[] args)
+    {
+        demo();
+    }
+}
