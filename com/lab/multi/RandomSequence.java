4962b880fd293a05adfd573fc1c2edf076652781
sauravMSFT <sauravsahu.12@live.com>
2016-10-17 20:42:48 +0530

    Finished multithreading.

--- com/lab/multi/RandomSequence.java
+++ com/lab/multi/RandomSequence.java
@@ -1,30 +1,51 @@
 package com.lab.multi;
 
-import java.util.Random;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.function.Supplier;
 
 /**
  * Created by saurav on 17/10/16.
  */
-public class RandomSequence implements Runnable
+public class RandomSequence<T> implements Runnable
 {
-    public RandomSequence(int count, String msg)
+    int count;
+    public ArrayList<T> array;
+    Supplier<T> generator;
+    Mode mode;
+
+    public RandomSequence(Supplier<T> gen, int count, Mode mode)
     {
+        this.array = new ArrayList<T>();
         this.count = count;
-        this.array = null;
-        this.message = msg;
+        this.generator = gen;
+        this.mode = mode;
     }
 
-    int count;
-    int[] array;
-    String message;
+    public RandomSequence(Supplier<T> gen, int count)
+    {
+        this(gen, count, Mode.SILENT);
+    }
 
     public void run()
     {
-        System.out.printf("%s started.\n", message);
-        Random random = new Random();
-        array = new int[count];
+        printDetails("Started");
         for (int i = 0; i < count; ++i)
-            array[i] = new Random().nextInt();
-        System.out.printf("%s ended.\n", message);
+        {
+            if (mode == Mode.VERBOSE) RandomSequence.printDetails("Iteration " + i);
+            array.add(generator.get());
+        }
+        printDetails("Ended");
+    }
+
+    public static void printDetails(String message)
+    {
+        System.out.printf("[Thread %s : Time %s : Log : %s]\n",
+                          Thread.currentThread().getName(),
+                          new SimpleDateFormat("HH:mm:ss.SSS")
+                                  .format(Calendar.getInstance()
+                                                  .getTime()),
+                          message);
     }
 }
--- com/lab/multi/Solution.java
+++ com/lab/multi/Solution.java
@@ -1,8 +1,44 @@
 package com.lab.multi;
 
+import java.util.Random;
+
 /**
  * Created by saurav on 17/10/16.
  */
 public class Solution
 {
+    public static void main(String[] args) throws Exception
+    {
+        int iter = 5;
+        Random random = new Random();
+        RandomSequence<Integer> integer =
+                new RandomSequence<>(
+                        () -> random.nextInt(100),
+                        iter);
+
+        RandomSequence<Float> floating =
+                new RandomSequence<>(
+                        () -> random.nextFloat(),
+                        iter);
+
+        RandomSequence<Character> chars =
+                new RandomSequence<>(
+                        () -> (char) (random.nextInt(26) + 'a'),
+                        iter);
+
+        Thread a = new Thread(integer, "A");
+        Thread b = new Thread(floating, "B");
+        Thread c = new Thread(chars, "C");
+
+        a.start();
+        b.start();
+        c.start();
+
+        a.join(); b.join(); c.join();
+
+        System.out.printf("A: %s\nB: %s\nC: %s",
+                          integer.array,
+                          floating.array,
+                          chars.array);
+    }
 }
